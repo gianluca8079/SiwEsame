05d7f50e663fd63c94f28d00981c82a654bcf916
gianluca8079 <92943708+gianluca8079@users.noreply.github.com>
2023-07-03 04:47:05 +0200

    SiwMovie

---
+++ siw-movie-03/src/main/java/it/uniroma3/siw/repository/RecensioneRepository.java
@@ -0,0 +1,23 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import it.uniroma3.siw.model.Recensione;
+
+public interface RecensioneRepository extends CrudRepository<Recensione, Long> {
+
+	@Query(value="select * "
+			+ "from recensione r "
+			+ "where r.film_recensito_id = :movieId"
+			+ " and r.utente_id = :userId", nativeQuery=true)
+	public Recensione existsFilmRecensitoIdAndUtenteId(@Param("movieId") Long idM,@Param("userId") Long idU); 
+		
+	
+	public boolean existsByTitleAndUtente(String title, String utente);	
+
+	
+
+
+}
---
+++ siw-movie-03/src/main/java/it/uniroma3/siw/service/CredentialsService.java
@@ -0,0 +1,57 @@
+package it.uniroma3.siw.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import it.uniroma3.siw.model.Credentials;
+import it.uniroma3.siw.repository.CredentialsRepository;
+
+@Service
+public class CredentialsService {
+	
+    @Autowired
+    protected PasswordEncoder passwordEncoder;
+
+	@Autowired
+	protected CredentialsRepository credentialsRepository;
+	
+	@Transactional
+	public Credentials getCredentials(Long id) {
+		Optional<Credentials> result = this.credentialsRepository.findById(id);
+		return result.orElse(null);
+	}
+
+	@Transactional
+	public Credentials getCredentials(String username) {
+		Optional<Credentials> result = this.credentialsRepository.findByUsername(username);
+		return result.orElse(null);
+	}
+		
+    @Transactional
+    public Credentials saveCredentials(Credentials credentials) {
+        credentials.setRole(Credentials.DEFAULT_ROLE);
+        credentials.setPassword(this.passwordEncoder.encode(credentials.getPassword()));
+        return this.credentialsRepository.save(credentials);
+    }
+
+    @Transactional
+	public Optional<Credentials> findByUsername(String user) {
+		return this.credentialsRepository.findByUsername(user);
+	}
+
+	public boolean alreadyUsed(String username) {
+		Iterable<Credentials> nomi = this.credentialsRepository.findAll();
+		for(Credentials n: nomi) {
+			if(n.getUsername().equals(username))
+				return true;
+		}
+		return false;
+	}
+    
+    
+    
+}
