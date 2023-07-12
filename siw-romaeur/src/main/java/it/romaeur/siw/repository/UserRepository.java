76d8407e809b13dbb9f271f76e3e569ab2b4591e
gianluca8079 <92943708+gianluca8079@users.noreply.github.com>
2023-07-12 14:11:17 +0200

    Delete siw-romaeur directory

--- siw-romaeur/src/main/java/it/romaeur/siw/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package it.romaeur.siw.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import it.romaeur.siw.model.User;
-
-
-public interface UserRepository extends CrudRepository<User, Long> {
-}
